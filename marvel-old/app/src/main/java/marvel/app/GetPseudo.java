e67ea4f51941da06c435e08ba61d8567fdbb484a
DEFINGS Adrien <adefings@saliege.fr>
2018-03-21 15:22:36 +0100

    Push(Pseudo+Mentions légales)
    Push(
    Pseudo
    +
    Mentions légales
    )

--- marvel-old/app/src/main/java/marvel/app/GetPseudo.java
+++ marvel-old/app/src/main/java/marvel/app/GetPseudo.java
@@ -6,6 +6,9 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -47,4 +50,47 @@ public class GetPseudo extends AppCompatActivity {
             photo.setImageBitmap(imageBitmap);
         }
     }
+
+    //Appel du menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    private void back(){
+        this.finish();
+    }
+    private void openSettingsUser(){
+        Intent pageSettingsUser = new Intent(GetPseudo.this, SettingsUser.class);
+        startActivity(pageSettingsUser);
+    }
+    private void openSettingsLaw(){
+        Intent pageSettingsLaw = new Intent(this, SettingsLaw.class);
+        startActivity(pageSettingsLaw);
+    }
+    private void openHome(){
+        Intent pageSettingsLaw = new Intent(this, MenuDemarrer.class);
+        startActivity(pageSettingsLaw);
+    }
+    //gère le click sur une action de l'ActionBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_back:
+                back();
+                return true;
+            case R.id.action_settings:
+                openSettingsUser();
+                return true;
+            case R.id.action_settings2:
+                openSettingsLaw();
+                return true;
+            case R.id.action_home:
+                openHome();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
