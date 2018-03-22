9c6b02741ab555d2910276c892c8e94fec3891fc
williamCourtade <courtade.william@gmail.com>
2018-03-22 14:49:52 +0100

    Get pseudo opérationnel
    Get pseudo opérationnel + changement dans colors.xml (rose en rouge)

--- marvel/app/src/main/java/marvel/app/GetPseudo.java
+++ marvel/app/src/main/java/marvel/app/GetPseudo.java
@@ -1,17 +1,32 @@
 package marvel.app;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 @SuppressLint("Registered")
 public class GetPseudo extends AppCompatActivity {
@@ -23,31 +38,116 @@ public class GetPseudo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_pseudo);
-
-
         Button btnCamera = (Button)findViewById(R.id.bt_photo);
         photo = (ImageView)findViewById(R.id.iv_photo);
-
+        //Appel de la fonction showPictureDialog au clic sur le bouton Camera
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //ouverture de la capture de photo
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                showPictureDialog();
             }
         });
     }
 
+    //Demande à l'utilisateur s'il veut prendre une photo ou en sélectionnez une dans sa galerie
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Sélectionner depuis la gallerie",
+                "Prendre une photo depuis la caméra" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    //Fonction permettant de sélectionnez une photo depuis la galerie
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, 2);
+    }
+
+    //Fonction permettant de prendre une photo
+    private void takePhotoFromCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //Fonction permettant de sauvegarder l'image que l'utilisateur vient de prendre
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + "MyMarvelHero");
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    //Gestion des erreurs / résultat de la selection de la photo
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        //Si la photo a bien été capturée
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            //on récupère l'image dans la variable imageBitmap
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //on change l'image sur la page
-            photo.setImageBitmap(imageBitmap);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == 2) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+                    Toast.makeText(GetPseudo.this, "Image enregistrée !", Toast.LENGTH_SHORT).show();
+                    photo.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(GetPseudo.this, "Une erreur s'est produite, veuillez recommencer!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                //on récupère l'image dans la variable imageBitmap
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                //on change l'image sur la page
+                photo.setImageBitmap(imageBitmap);
+            }
         }
     }
 
